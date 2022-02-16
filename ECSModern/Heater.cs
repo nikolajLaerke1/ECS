1146102e3724f9585e0f9d134ea473da0e39b4f9
LukasM <au617683@post.au.dk>
2022-02-16 15:59:09 +0100

    Add Modern ECS

---
+++ ECSModern/Heater.cs
@@ -0,0 +1,22 @@
+﻿using ECSModern;
+
+namespace ECS.Legacy
+{
+    public class Heater : IHeater
+    {
+        public void TurnOn()
+        {
+            System.Console.WriteLine("Heater is on");
+        }
+
+        public void TurnOff()
+        {
+            System.Console.WriteLine("Heater is off");
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
